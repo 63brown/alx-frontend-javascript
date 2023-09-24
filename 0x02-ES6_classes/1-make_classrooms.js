a88d14d16a027add4dbfa109b57cd3aa263452cb
63brown <mutheemumbi01@gmail.com>
2023-09-24 22:48:34 +0300

    returns an array of 3 ClassRoom objects with the sizes 19, 20, and 34 (in this order)

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,6 +4,6 @@ export default function initializeRooms() {
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
   const class3 = new ClassRoom(34);
-  ClassRoom.push(class1, class2, class3);
-  return ClassRoom;
+  ClassRoomLi.push(class1, class2, class3);
+  return ClassRoomLi;
 }
