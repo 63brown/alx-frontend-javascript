3a3c9c9a7a0d1a9dbbd51a719c413ca2826c13c2
63brown <mutheemumbi01@gmail.com>
2023-09-24 21:53:24 +0300

    return the number of arguments passed to it

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,3 @@
-export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-  this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
-  };
+export default function returnHowManyArguments(...theArgs) {
+  return theArgs.length;
 }
